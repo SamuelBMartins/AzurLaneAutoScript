e93b3795e50484393e91b8b179d4c386c5e5cd01
Samuel Martins <s@smartins.ch>
2022-07-27 12:15:02 +0200

    Increase timer stuck

--- module/device/device.py
+++ module/device/device.py
@@ -24,7 +24,7 @@ class Device(Screenshot, Control, AppControl, EmulatorManager):
     detect_record = set()
     click_record = deque(maxlen=15)
     stuck_timer = Timer(60, count=60).start()
-    stuck_timer_long = Timer(180, count=180).start()
+    stuck_timer_long = Timer(210, count=210).start()
     stuck_long_wait_list = ['BATTLE_STATUS_S', 'PAUSE', 'LOGIN_CHECK', 'AUTO_SEARCH_MENU_CONTINUE']
 
     def __init__(self, *args, **kwargs):
